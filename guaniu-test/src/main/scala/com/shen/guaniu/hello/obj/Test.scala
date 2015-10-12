d06ebe8c9f041661d07d91482537315981d2b052
dimhunter <43543865@qq.com>
2015-10-12 18:27:00 +0800

    添加object和伴生对象部分以及apply方法

---
+++ guaniu-test/src/main/scala/com/shen/guaniu/hello/obj/Test.scala
@@ -0,0 +1,17 @@
+package com.shen.guaniu.hello.obj
+
+/**
+ * Created by shenluguo on 15/10/12.
+ */
+object Test {
+  def main(args: Array[String]) {
+    //没有参数的方法调用可以不写()
+    ObjectTest.staticMethod
+    println(ObjectTest.name)
+
+    new BanShengDemo("aa","bb").test1()
+
+    //apply方法，有了apply方法，就可以如下方式new了
+    BanShengDemo("aaaaa")
+  }
+}
