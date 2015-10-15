586b1840e32a3ed56fea4293dd9a64ab08826ab5
dimhunter <43543865@qq.com>
2015-10-15 18:20:06 +0800

    匿名函数

---
+++ guaniu-test/src/main/scala/com/shen/guaniu/hello/function/BiBaoTest.scala
@@ -0,0 +1,20 @@
+package com.shen.guaniu.hello.function
+
+/**
+ * 闭包测试？？？？例子不懂
+ * Created by shenluguo on 15/10/15.
+ */
+object BiBaoTest {
+
+  def mulBy(factor:Double) = (x:Double) => factor * x
+
+  def main(args: Array[String]) {
+    val triple = mulBy(3)
+    val half = mulBy(0.5)
+
+    println(triple+"=="+half)
+
+    println(triple(14) + " -- " + half(14))
+  }
+
+}
---
+++ guaniu-test/src/main/scala/com/shen/guaniu/hello/function/NimingFun.scala
@@ -0,0 +1,18 @@
+package com.shen.guaniu.hello.function
+
+/**
+ * Created by shenluguo on 15/10/15.
+ */
+object NimingFun {
+
+  //scala中，不需要给每个函数命名，这就是个匿名函数，该函数将传给他的参数乘3
+  (x:Double) => 3*x
+
+  //也可以将函数存到变量中，这就跟用def定义是一样的。def cheng4(x:Double) = 4*x
+  val cheng4 = (x:Double) => 4*x
+
+
+  //也可以将匿名函数直接传递给另一个函数，这里告诉map方法，将每个元素乘2
+  Array(1,2,5).map((x:Int) => 2*x)
+
+}
