9246a8df10933ee3043d58f4588d3e8841754661
dimhunter <43543865@qq.com>
2015-12-09 18:33:57 +0800

    添加内容

---
+++ guaniu-test/src/main/scala/com/shen/guaniu/hello/function/ValueFun.scala
@@ -0,0 +1,19 @@
+package com.shen.guaniu.hello.function
+
+/**
+ * Created by shenge on 15/12/9.
+ *
+ * 作为值的函数
+ */
+object ValueFun {
+
+  val num = 0
+  // aaa后的 _ 意味着你确实指的是aaa这个函数，而不是碰巧忘了传参数
+  val funa = aaa _
+
+
+  def aaa: Unit ={
+
+  }
+
+}
