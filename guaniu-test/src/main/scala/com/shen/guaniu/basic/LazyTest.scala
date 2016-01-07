d9e3e2f6ede0e7c5e81945755cabfd5a5effe706
dimhunter <43543865@qq.com>
2016-01-07 14:55:23 +0800

    zhengli

--- guaniu-test/src/main/scala/com/shen/guaniu/basic/LazyTest.scala
+++ guaniu-test/src/main/scala/com/shen/guaniu/basic/LazyTest.scala
@@ -9,7 +9,7 @@ object LazyTest {
     //当val声明为lazy时，它的初始化会被推迟，知道首次对他进行取值时才会进行初始化
     //这里是读一个不存在的文件，故意写错文件名，如果是lazy，并不会报错,直到下面调用file.getLines取值时才会报错。
     //如果删掉lazy，会直接报错。
-    lazy val file = Source.fromFile("E:\\WangJlisssfsdalin.txt")
+    lazy val file = Source.fromFile("D:\\noExistFile.txt")
     
     println("Scala")
     
